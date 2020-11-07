76db97c50fe4e67ec3e29883f20e79160963c856
namdam1231 <yuco109@gmail.com>
2020-11-07 17:27:58 +0900

    character and Camera move

---
+++ EscapeFrom/Assets/Scripts/cameraCtrl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraCtrl : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0f, 20f)]
+    float m_distance;
+    [SerializeField]
+    [Range(0f, 20f)]
+    float m_height;
+    [SerializeField]
+    [Range(0f, 180f)]
+    float m_angle;
+    [SerializeField]
+    [Range(0.2f, 10f)]
+    float m_speed;
+    [SerializeField]
+    Transform m_target;
+
+    Transform m_prevTransform;
+
+    void UpdatePosition()
+    {
+        transform.position = new Vector3(Mathf.Lerp(m_prevTransform.position.x, m_target.position.x, m_speed * Time.deltaTime),
+                                         Mathf.Lerp(m_prevTransform.position.y, m_target.position.y + m_height, m_speed * Time.deltaTime),
+                                         Mathf.Lerp(m_prevTransform.position.z, m_target.position.z - m_distance, m_speed * Time.deltaTime));
+        transform.eulerAngles = new Vector3(Mathf.Lerp(m_prevTransform.eulerAngles.x, m_angle, m_speed * Time.deltaTime), 0f, 0f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_prevTransform = m_target;
+        UpdatePosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void LateUpdate()
+    {
+        m_prevTransform = transform;
+    }
+}
