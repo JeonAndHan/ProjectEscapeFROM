a51566588468fd86cc0841e9a8d35a118f28837d
namdam1231 <yuco109@gmail.com>
2020-11-10 21:37:46 +0900

    animation finish
    Boss Zombie, Zombie, remy animation finish

---
+++ EscapeFrom/Assets/Scripts/BossZombie.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossZombie : MonoBehaviour
+{
+
+    [SerializeField]
+    float m_speed;
+    [SerializeField]
+    Vector3 m_dir;
+
+    Rigidbody m_rigidbody;
+    CapsuleCollider m_collider;
+    Animator m_Anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_collider = GetComponent<CapsuleCollider>();
+        m_Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
